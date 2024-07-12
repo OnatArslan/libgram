5b4637fe689828074209ba0d0db9e7d505aa4249
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-07-12 09:46:22 +0300

    Commit for change server

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const app = require("./app");
 
 // RUN SERVER ---------------------------
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+try {
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+} catch (error) {
+  console.error(error.message);
+}
