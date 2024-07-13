21208dd497f52e35cacaecb40c62aac6f48c8268
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-07-13 08:46:37 +0300

    Commit for double assations

--- models/database.js
+++ models/database.js
@@ -20,5 +20,15 @@ module.exports = sequelize;
 const User = require(`./userModel`);
 const Book = require(`./bookModel`);
 
-User.belongsToMany(Book, { through: `userBook`, as: `books` });
-Book.belongsToMany(User, { through: `userBook`, as: `owners` });
+User.belongsToMany(Book, {
+  through: { model: "userbook", unique: false },
+  as: "books",
+  foreignKey: "userId",
+  otherKey: "bookId",
+});
+Book.belongsToMany(User, {
+  through: { model: "userbook", unique: false },
+  as: "owners",
+  foreignKey: "bookId",
+  otherKey: "userId",
+});
