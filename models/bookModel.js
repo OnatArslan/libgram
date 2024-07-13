8a80096688c6d615d6eff61dfcb11ea3cc03eb0f
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-07-13 03:27:21 +0300

    Commit for enchanging book model

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,6 +1,6 @@
 const { Sequelize, DataTypes, Model } = require(`sequelize`);
 const sequelize = require(`./database`);
-const validatorjs = require("validatorjs");
+const validator = require("validator");
 
 const Book = sequelize.define(
   `Book`,
@@ -15,10 +15,18 @@ const Book = sequelize.define(
       type: DataTypes.STRING(100),
       unique: true,
       allowNull: false,
+      validate: {
+        min: 3,
+        max: 50,
+      },
     },
     isbn: {
       type: DataTypes.INTEGER,
       unique: true,
+      allowNull: true, // this must be false but for production best like this
+      validate: {
+        // isISBN: true, This is not neccessary for now COME BACK LATER
+      },
     },
   },
   {
