b76bdc1a62ddcebb7a1c867adcc1b2e663d93de6
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-07-21 14:38:24 +0300

    Commit for changes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -63,7 +63,7 @@ exports.getUser = async (req, res, next) => {
 exports.createUser = async (req, res, next) => {
   try {
     if (!(password === passwordConfirmation)) {
-      return next(new Error(`Password and confirmation does not match!!`));
+      return next(new Error(`Password and confirmation does not match!!!`));
     }
     const userData = {
       username: req.body.username,
